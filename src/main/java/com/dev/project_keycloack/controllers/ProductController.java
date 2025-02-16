60315e87190aead686da1a9af78cf28f7a9627a8
nabousoum <seynabousoumare98@outlook.fr>
2025-02-16 01:31:27 +0000

    modifs sur product

--- src/main/java/com/dev/project_keycloack/controllers/ProductController.java
+++ src/main/java/com/dev/project_keycloack/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.dev.project_keycloack.controllers;
 import com.dev.project_keycloack.entities.Product;
 import com.dev.project_keycloack.services.IProduct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +21,22 @@ public class ProductController {
 
     @GetMapping
     public String getProducts(ModelMap model) {
-//        OAuth2AuthenticationToken authentication =
-//                (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof JwtAuthenticationToken)) {
+            model.addAttribute("error", "L'utilisateur n'est pas authentifié !");
+            return "error";
+        }
+
+        JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
         var list = productService.getProducts();
         model.addAttribute("products", list);
         model.addAttribute("product", new Product());
-//        model.addAttribute("username", authentication.getPrincipal().getAttribute("preferred_username"));
-//        model.addAttribute("name", authentication.getPrincipal().getAttribute("name"));
+        model.addAttribute("username", jwtToken.getTokenAttributes().get("preferred_username"));
+        model.addAttribute("name", jwtToken.getTokenAttributes().get("name"));
         return "product";
     }
+
 
     @PostMapping
     public String addProduct(ModelMap model, @ModelAttribute Product product) {
